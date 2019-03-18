2640e9a408215a5a0e1e3ce6b3bbff9e6fbc47d3
Calvin Kwong <calvin.kwong17@stjohns.edu>
2019-03-18 01:07:50 -0400

    users can logout

--- blueprints/users.py
+++ blueprints/users.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, redirect, url_for, request
-from flask_login import current_user, login_user
+from flask_login import current_user, login_user, logout_user
 from models import User, db
 
 users = Blueprint('users', __name__, template_folder="templates")
@@ -30,3 +30,8 @@ def signup():
         db.session.commit()
         return redirect(url_for('home_page.index'))
     return render_template('signup.html', error=False)
+
+@users.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home_page.index'))
