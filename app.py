020063dd34f5597853c78ff4a35c29f9835f2e64
Calvin Kwong <calvin.kwong17@stjohns.edu>
2019-03-18 14:59:54 -0400

    error handling pages

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import sys
-from flask import Flask
+from flask import Flask, render_template
 from flask_login import LoginManager
 from blueprints.home import home_page
 from blueprints.users import users
 from blueprints.recipe import recipes
+from blueprints.errors import errors
 from config import Config
 from models import db, User
 
@@ -20,7 +21,11 @@ def load_user(id): # setting users to sessions
 app.register_blueprint(home_page)
 app.register_blueprint(users)
 app.register_blueprint(recipes)
+app.register_blueprint(errors)
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('errors/404.html'), 404
 
 def main():
     if (len(sys.argv)==2):
