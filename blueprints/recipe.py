38c4809550f25703d2e8618804fc415c0c1819d0
nabjav <46038729+nabjav@users.noreply.github.com>
2019-04-06 00:19:52 -0400

    For pull

--- blueprints/recipe.py
+++ blueprints/recipe.py
@@ -1,9 +1,11 @@
 from flask import Blueprint, render_template
+from models import User, db, Filter, Recipe, Ingredient
+
 
 recipes = Blueprint('recipes', __name__, template_folder="templates")
 
 
 @recipes.route('/recipe/<recipe_id>')
-def show_recipe():
-    # get search results
-    return render_template('recipe.html')
+def show_recipe(recipe_id):
+    recipe = Recipe.query.filter_by(recipe_id=recipe_id).first()
+    return render_template('recipe.html', recipe=recipe)
